0d3f1169b196867d9755b622634bc1807b446ad9
Ranjan <skillsapphire@gmail.com>
2021-10-04 19:08:54 +0200

    initial commit

---
+++ database/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //db: 'mongodb://localhost:27017/reactdb'
+  db: 'mongodb://mernsms:qxaPSiychwHek4bCchBIUA7QN3apL5xLzpH6oY7jCA8pfogjUfVG49bQQ8IOwWOVRhvQFIVEazByYATLnaomtg==@mernsms.mongo.cosmos.azure.com:10255/mern-sms?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@mernsms@'
+};
